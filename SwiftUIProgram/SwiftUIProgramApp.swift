e1ea8d77f7b24499c1474a9e843970420e9b48b5
lazersly <brandonjroberts@yahoo.com>
2023-03-22 13:20:26 -0700

    Initial Commit

---
+++ SwiftUIProgram/SwiftUIProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProgramApp.swift
+//  SwiftUIProgram
+//
+//  Created by Brandon Roberts on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
